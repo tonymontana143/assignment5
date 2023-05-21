76f779c379b30825f1b42c995e4360617badc46c
TonyMontana143 <221074@astanait.edu.kz>
2023-05-21 17:55:56 +0600

    init

---
+++ src/BST.java
@@ -0,0 +1,30 @@
+public class BST<K extends Comparable<K>, V> {
+    private Node root;
+
+    private class Node {
+        private K key;
+        private V val;
+        private Node left, right;
+
+
+        public Node(K key, V val) {
+            this.key = key;
+            this.val = val;
+        }
+    }
+
+    public void put()
+    public void put(K key, V val) {
+
+    }
+
+
+    public V get(K key) {
+    }
+
+    public void delete(K key) {
+    }
+
+    public Iterable<K> iterator() {
+    }
+}
